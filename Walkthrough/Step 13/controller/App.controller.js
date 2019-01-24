f48722c3f8272dfe673894c2e9563dc12202d97d
Aliona Sauchuk <aliona.sauchuk@LeverX.local>
2019-01-24 11:15:22 +0300

    Step 13

---
+++ Walkthrough/Step 13/controller/App.controller.js
@@ -0,0 +1,18 @@
+sap.ui.define([
+   "sap/ui/core/mvc/Controller"
+], function (Controller) {
+   "use strict";
+   return Controller.extend("courses.controller.App", {
+      onOpenDialog : function () {
+			this.getOwnerComponent().openHelloDialog();
+      },
+     onShowHello : function () {
+      // read msg from i18n model
+      var oBundle = this.getView().getModel("i18n").getResourceBundle();
+      var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+      var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+      // show message
+      MessageToast.show(sMsg);
+     }
+   });
+});
